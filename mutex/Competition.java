bd11703211ea1876face9552b2ac9a2e20eac8ed
gopuvarsha <gopu_varsha@yahoo.com>
2013-11-10 12:21:42 +0530

    Update Competition.java

--- mutex/Competition.java
+++ mutex/Competition.java
@@ -7,55 +7,60 @@ import java.util.HashMap;
 class Competition extends UnicastRemoteObject implements CompetitionInterface {
 
 
-int noOfParticipants;			  // number of participants in the competition
-int noOfResources;				  // total number of resources
-Map<int, String> studentsMap;				    // list of students and their IP addresses
-Resource resource_list[4];				  // list of resources 
+  int noOfParticipants;			         // number of participants in the competition
+  int noOfResources;				         // total number of resources
+  Map<int, String> studentsMap;			 // list of students and their IP addresses
+  Resource resource_list[4];				 // list of resources 
 
-public:
+  public:
 
-Competition() {
+  Competition() {
 
-noOfParticipants = 0;
-noOfResources = 4;
+    noOfParticipants = 0;
+    noOfResources = 4;
+    studentsMap = new HashMap<int,String>();
+    resource_list[0]=new Resource('Pencil','true',NONSHARED);
+    resource_list[1]=new Resource('Chart','true',NONSHARED);
+    resource_list[2]=new Resource('Scale','true',NONSHARED);
+    resource_list[3]=new Resource('Eraser','false',SHARED);
+  
+  }
+  
+  
+  void startCompetition() {
+    String stu_ip;
+    for (String value : students.values()) {
+      stu_ip = value;
+      StudentInterface student=(StudentInterface)Naming.lookup("//" +stu_ip + "/StudentInterface");
+      student.AssignStudentsMap(studentsMap);
+    }
+  }		
+  
+   // Use to allocate unique ID to students (processes) 
 
-studentsMap = new HashMap<int,String>();
-resource_list[0]=new Resource('Pencil','true',NONSHARED);
-resource_list[1]=new Resource('Chart','true',NONSHARED);
-resource_list[2]=new Resource('Scale','true',NONSHARED);
-resource_list[3]=new Resource('Eraser','false',SHARED);
+  int getId(String ip) throws RemoteException {
 
-
-}
-void startCompetition() {
-String stu_ip;
-for (String value : students.values()) {
-  stu_ip = value;
-  StudentInterface student=(StudentInterface)Naming.lookup("//" +stu_ip + "/StudentInterface");
-  student.AssignStudentsMap(studentsMap);
-
-
-}
-}		
-int getId(String ip) throws RemoteException {
-
-try{	
-	noOfParticipants += 1;
-	studentsMap.put(noOfParticipants, ip);
-	} catch (Exception e) { System.out.println("Exception:" + e.getMessage()) }
-	return noOfParticipants;
+    try{	
+	    noOfParticipants += 1;
+	    studentsMap.put(noOfParticipants, ip);
+	  } catch (Exception e) { System.out.println("Exception:" + e.getMessage()) }
 	
-}
+	return noOfParticipants;
+	}
 
-    
-           // Use to allocate unique ID to students (processes) 
-Resource[] getResources throws RemoteException() {
+      
+  // Allocates resources to each student
+          
+  Resource[] getResources throws RemoteException() {
 
- return resource_list;
-}		  // Allocates resources to each student
-HashMap getStudents throws RemoteException() {
+    return resource_list;
+  }		  
+  
+  // Shares list of Students along with their ip addresses
+  
+  HashMap getStudents throws RemoteException() {
 
-return studentsMap;
-}
+  return studentsMap;
+  }
 
 }
